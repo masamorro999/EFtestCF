8792819dc07ca150f5878ebd82ca4a222e110b26
msierra <msierra@MSIERRA>
2017-03-23 10:38:26 -0500

    asdasd

---
+++ Jupiter/Jupiter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jupiter.Startup))]
+namespace Jupiter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
